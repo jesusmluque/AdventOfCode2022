09d54334e4c8a358c7a04d9059dad81ae11d1912
jesus <jesusmluque@gmail.com>
2022-12-11 20:01:25 +0100

    Day 10: Adding Tagless final approach

--- src/main/scala/CathodeRayTube.scala
+++ src/main/scala/CathodeRayTube.scala
@@ -1,3 +1,4 @@
+
 object CathodeRayTube:
 
   def getSumSygnalStrengthAfterInstruction(rawInstruction: List[String]) =
@@ -13,7 +14,7 @@ object CathodeRayTube:
       case pattern(value) => (value.toLong + acc.head) :: acc.head :: acc
     }
     }.reverse.toVector
-
+  
   def drawCRT(register5History: Vector[Long]) =
     def drawRow(range: Range) =
       range.foreach { cycle =>
@@ -34,3 +35,51 @@ object CathodeRayTube:
     drawRow(160 to 199)
     System.out.print("\n")
     drawRow(200 to 239)
+    
+    
+/*--------------------------------------------DrawCRT IO pure without side effect-------------------------------------------------*/
+  //Alternative approach to use IO Monad having a referential transparency version of the drawCRT method, that now is pure
+  //First, Monad and Console monads:
+  trait Monad[F[_]] {
+    def pure[A](a: A):F[A]
+    def map[A,B](fa: F[A])(f: A => B):F[B]
+    def flatMap[A,B](fa: F[A])(f: A => F[B]): F[B]
+  }
+  implicit class monadOps[F[_]: Monad,A](fa: F[A]) {
+    def flatMap[B](f: A => F[B]): F[B] = implicitly[Monad[F]].flatMap(fa)(f)
+    def map[B](f: A => B): F[B] = implicitly[Monad[F]].map(fa)(f)
+
+  }
+  object Monad {
+    def pure[F[_]: Monad, A](a: A):F[A] = implicitly[Monad[F]].pure(a)
+  }
+
+  trait Console[F[_]] {
+    def putStrLn(str: String): F[Unit]
+  }
+
+  def putStrLn[F[_]: Console](str: String) = implicitly[Console[F]].putStrLn(str)
+
+  //new drawCRT method returning a generic Adt F that represent the console output without side effect.
+  def drawCRTIO[F[_]: Monad: Console](register5History: Vector[Long]):F[Unit] =
+    def drawRow(range: Range) =
+      putStrLn(range.map { cycle =>
+        val currentValue = register5History(cycle)
+        if cycle + 1 - range.head < currentValue + 3 && cycle + 1 - range.head >= currentValue then
+          "#"
+        else
+          "·"
+      }.mkString(""))
+    for {
+      _ <- drawRow(0 to 39)
+      _ <- putStrLn("\n")
+      _ <- drawRow(40 to 79)
+      _ <- putStrLn("\n")
+      _ <- drawRow(80 to 119)
+      _ <- putStrLn("\n")
+      _ <- drawRow(120 to 159)
+      _ <- putStrLn("\n")
+      _ <- drawRow(160 to 199)
+      _ <- putStrLn("\n")
+      _ <- drawRow(200 to 239)
+    } yield ()
--- src/test/scala/CathodeRayTubeTest.scala
+++ src/test/scala/CathodeRayTubeTest.scala
@@ -1,3 +1,4 @@
+import CathodeRayTube.calculateInstructions
 import org.scalatest.flatspec.AnyFlatSpec
 
 import scala.io.Source
@@ -10,5 +11,33 @@ class CathodeRayTubeTest extends AnyFlatSpec {
 
   "The sum of the signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th cycles for the cathode-ray-tube2 file " should " be 14520 " in {
     assert(CathodeRayTube.getSumSygnalStrengthAfterInstruction(Source.fromResource("cathode-ray-tube2").getLines().toList) == 14520L)
+  }
+
+
+  /*-------------------------------------------DrawCRT IO pure without side effect-------------------------------------------------*/
+
+  //Creating the type class instance for Monad and Console ADTs through the IO monad
+  //The IO implement both, Console and Monad type classes and use the system.out to print
+  //It is possible to have more than one implementation of the Console and Monad due to the generic ADT F
+  //We can have a special F insntace for test where we could compare the result instead of printing it. We are splitting
+  //the behavior of the program logic from the way the result is print it or even sent or any other side effect.
+  case class IO[A](performEff: () => A)
+  object IO:
+    def pure[A](a: A): IO[A] = IO(() => a)
+
+    given ioMonad: CathodeRayTube.Monad[IO] with
+      override def pure[A](a: A): IO[A] = IO.pure(a)
+      override def map[A, B](fa: IO[A])(f: A => B): IO[B] = IO(() => f(fa.performEff()))
+      override def flatMap[A, B](fa: IO[A])(f: A => IO[B]): IO[B] = IO(() => f(fa.performEff()).performEff())
+
+    given consoleIO: CathodeRayTube.Console[IO] with
+      override def putStrLn(str: String): IO[Unit] = IO(() => System.out.print(str))
+
+
+  "Tagless Final execution example " should " be " in {
+    val result = calculateInstructions(Source.fromResource("cathode-ray-tube2").getLines().toList)
+    //here is where the side effect really happen (perfromEFF)
+    CathodeRayTube.drawCRTIO[IO](result).performEff()
+    assert(true)
   }
 }
